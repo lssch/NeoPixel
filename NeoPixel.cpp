26956ecbd1ddcad349ace8a60180dc44c5fe5e05
lssch <ls.schwarz@outlook.com>
2023-11-06 20:53:10 +0100

    Added support for groups

--- NeoPixel.cpp
+++ NeoPixel.cpp
@@ -2,56 +2,89 @@
 // Created by Lars Schwarz on 13.10.2023.
 //
 
+
+// TODO: std::array<LedClass, numLedds> leds
+
 #include <vector>
 #include "NeoPixel.h"
 
 NeoPixel::Pixel::Pixel() {
   off();
+  set_brightness(100);
 }
 
 void NeoPixel::Pixel::off() {
-  red = 0;
-  green = 0;
-  blue = 0;
-  brightness = 100;
+  color.red = 0;
+  color.green = 0;
+  color.blue = 0;
 }
 
-void NeoPixel::Pixel::set_color(uint8_t red_, uint8_t green_, uint8_t blue_) {
-  red = red_;
-  green = green_;
-  blue = blue_;
+void NeoPixel::Pixel::set_color(uint8_t red, uint8_t green, uint8_t blue) {
+  color.red = red;
+  color.green = green;
+  color.blue = blue;
 }
 
-void NeoPixel::Pixel::set_brightness(uint8_t brightness_) {
-  brightness = brightness_;
+void NeoPixel::Pixel::set_brightness(uint8_t brightness) {
+  color.brightness = brightness;
 }
 
-NeoPixel::Controller::Controller(TIM_HandleTypeDef *htim_, uint8_t channel_, type_e type_, uint8_t number_of_pixels) {
-  htim = htim_;
-  channel = channel_;
-  type = type_;
-  add_pixel(number_of_pixels);
+NeoPixel::Pixel::color_t NeoPixel::Pixel::getColor() {
+  return color;
 }
 
-void NeoPixel::Controller::add_pixel(uint8_t number_of_pixels) {
-  NeoPixel::Pixel pixel;
-  for (uint8_t i = 0; i < number_of_pixels; ++i)
-    pixels.push_back(pixel);
+NeoPixel::Group::Group(std::initializer_list<Pixel *> pixels_): pixels(pixels_) {}
+
+NeoPixel::Group::Group(NeoPixel::Pixel *pixels_, uint8_t number_of_pixels) {
+  for (int i = 0; i < number_of_pixels; ++i) {
+    pixels.push_back((pixels_+i));
+  }
+}
+
+void NeoPixel::Group::off() {
+  for (auto &pixel: pixels)
+    pixel->off();
+}
+
+void NeoPixel::Group::set_color(uint8_t red, uint8_t green, uint8_t blue) {
+  for (auto &pixel: pixels)
+    pixel->set_color(red, green, blue);
+}
+
+void NeoPixel::Group::set_brightness(uint8_t brightness) {
+  for (auto &pixel: pixels)
+    pixel->set_brightness(brightness);
+}
+
+std::vector<NeoPixel::Pixel*> NeoPixel::Group::get_pixels() {
+  return pixels;
+}
+
+NeoPixel::Controller::Controller(NeoPixel::Controller::timer_t timer_, NeoPixel::Controller::type_e type_,
+                                 std::initializer_list<Group *> groups_)
+ : timer(timer_),
+ type(type_){
+
+  for (auto &group: groups_) {
+    for (const auto &pixel: group->get_pixels()) {
+      pixels.push_back(pixel);
+    }
+  }
 }
 
 void NeoPixel::Controller::off() {
   for (auto &pixel: pixels)
-    pixel.off();
+    pixel->off();
 }
 
 void NeoPixel::Controller::set_color(uint8_t red, uint8_t green, uint8_t blue) {
   for (auto &pixel: pixels)
-    pixel.set_color(red, green, blue);
+    pixel->set_color(red, green, blue);
 }
 
 void NeoPixel::Controller::set_brightness(uint8_t brightness) {
   for (auto &pixel: pixels)
-    pixel.set_brightness(brightness);
+    pixel->set_brightness(brightness);
 }
 
 void NeoPixel::Controller::update() {
@@ -59,18 +92,18 @@ void NeoPixel::Controller::update() {
   dma_buffer.clear();
   uint32_t color;
   for (auto &pixel: pixels) {
-    color = (((pixel.green*pixel.brightness/100) << 16)
-            | ((pixel.red*pixel.brightness/100) << 8)
-            | (pixel.blue*pixel.brightness/100));
+    color = (((pixel->getColor().green * pixel->getColor().brightness / 100) << 16)
+            | ((pixel->getColor().red * pixel->getColor().brightness / 100) << 8)
+            | (pixel->getColor().blue * pixel->getColor().brightness / 100));
     for (int i = 23; i >=0 ; i--)
       dma_buffer.push_back((color&(1<<i)) ? LOGIC_ONE : LOGIC_ZERO);
   }
   dma_buffer.push_back(0);
-  HAL_TIM_PWM_Start_DMA(htim, channel, dma_buffer.data(), dma_buffer.size());
+  HAL_TIM_PWM_Start_DMA(timer.htim, timer.channel, dma_buffer.data(), dma_buffer.size());
   dma_active = true;
 }
 
 void NeoPixel::Controller::dma_finished() {
-  HAL_TIM_PWM_Stop_DMA(htim, TIM_CHANNEL_1);
+  HAL_TIM_PWM_Stop_DMA(timer.htim, TIM_CHANNEL_1);
   dma_active = false;
 }
--- NeoPixel.h
+++ NeoPixel.h
@@ -7,6 +7,7 @@
 
 #include "stm32f4xx_hal.h"
 #include <vector>
+#include <array>
 
 namespace NeoPixel{
   #define LOGIC_ZERO 25
@@ -14,36 +15,59 @@ namespace NeoPixel{
 
   class Pixel {
   public:
+    typedef struct _color_t {
+      uint8_t red;
+      uint8_t green;
+      uint8_t blue;
+      uint8_t brightness;
+    } color_t;
+
     Pixel();
     void off();
-    void set_color(uint8_t, uint8_t, uint8_t);
-    void set_brightness(uint8_t);
-    uint8_t red{};
-    uint8_t green{};
-    uint8_t blue{};
-    uint8_t brightness{};
+    void set_color(uint8_t red, uint8_t green, uint8_t blue);
+    color_t getColor();
+    void set_brightness(uint8_t brightness);
+
   private:
+    color_t color;
+  };
+
+  class Group {
+  public:
+    Group(std::initializer_list<Pixel*> pixels_);
+    Group(Pixel* pixels, uint8_t number_of_pixels);
+    void off();
+    void set_color(uint8_t red, uint8_t green, uint8_t blue);
+    void set_brightness(uint8_t brightness);
+    std::vector<Pixel*> get_pixels();
+
+  private:
+    std::vector<Pixel*> pixels;
   };
 
   class Controller {
   public:
-    typedef enum {
+    typedef enum _type_e {
       WS2812 = 0,
       SK6812
     } type_e;
 
-    Controller(TIM_HandleTypeDef *htim_, uint8_t channel_, type_e type_, uint8_t number_of_pixels);
-    void add_pixel(uint8_t number_of_pixels);
+    typedef struct _timer_t {
+      TIM_HandleTypeDef *htim;
+      uint8_t channel;
+    } timer_t;
+
+    Controller(timer_t timer_, type_e type_, std::initializer_list<Group *> pixels_);
+
     void off();
-    void set_color(uint8_t, uint8_t, uint8_t);
-    void set_brightness(uint8_t);
+    void set_color(uint8_t red, uint8_t green, uint8_t blue);
+    void set_brightness(uint8_t brightness);
     void update();
     void dma_finished();
-    std::vector<Pixel> pixels;
   private:
-    TIM_HandleTypeDef *htim;
-    uint8_t channel;
+    timer_t timer;
     type_e type;
+    std::vector<Pixel*> pixels;
     bool dma_active{false};
     std::vector<uint32_t> dma_buffer;
   };
